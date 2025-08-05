d3e91beefe9eb4de62eb239309b5b0362aa47b7c
wborgwedel <w.borgwedel@gmail.com>
2025-08-05 22:41:52 +0200

    Set `DEBUG` to `False` for production readiness.

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7j(@z8g0qc0hsl3wiqp55_ult3k3g&lh17@643@*g_q=sikrxr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['168.119.168.45']
 
