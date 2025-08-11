089b2ace8d756c3731de496b26ef9389811935a7
wborgwedel <w.borgwedel@gmail.com>
2025-08-11 15:37:56 +0200

    Update `STATIC_ROOT` and add WhiteNoise for static file handling

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -120,8 +120,11 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = Path('/app/staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = Path('/app/staticfiles')
+
+# WhiteNoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
