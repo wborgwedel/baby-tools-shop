ebcb4682d9727be5c84caeb7bbca41787e1d9347
wborgwedel <w.borgwedel@gmail.com>
2025-08-11 15:11:51 +0200

    Update `STATIC_ROOT` to point to `/app/staticfiles` for deployment

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = Path('/app/staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
