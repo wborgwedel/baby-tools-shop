4ff5d01a79dd20cf593a8aeb1a1581e103821f6c
wborgwedel <w.borgwedel@gmail.com>
2025-08-13 12:30:38 +0200

    Make secret key configurable via environment variable in `settings.py` and update `example.env`.

--- babyshop_app/babyshop/settings.py
+++ babyshop_app/babyshop/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7j(@z8g0qc0hsl3wiqp55_ult3k3g&lh17@643@*g_q=sikrxr'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False") == "True"
